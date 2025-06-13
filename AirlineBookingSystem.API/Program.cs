5acfba1e3cb6d3e5dbb19be81e9bf1734579a19a
akram <akram7.dris7@gmail.com>
2025-06-13 10:57:42 +0100

    feat(infrastructure): add Infrastructure layer with dependency injection setup
    - Added new Infrastructure project reference for Microsoft.Extensions.Configuration.Abstractions
    - Implemented AddInfrastructure extension method for service registration
    - Integrated Infrastructure layer into API startup configuration
    - Prepared structure for future infrastructure service registrations

--- AirlineBookingSystem.API/Program.cs
+++ AirlineBookingSystem.API/Program.cs
@@ -2,6 +2,7 @@ using AirlineBookingSystem.Persistence;
 using DotNetEnv;
 using System.IO;
 using AirlineBookingSystem.Application;
+using AirlineBookingSystem.Infrastructure;
 
 // Load environment variables from .env file in current directory
 Env.Load();
@@ -9,6 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Configuration
     .AddEnvironmentVariables();
 builder.Services.AddPersistence(builder.Configuration);
+builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddApplication();
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
