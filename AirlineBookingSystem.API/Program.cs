6e1622dd1ce317f1b3a4c84f94252571d2566c8b
akram <akram7.dris7@gmail.com>
2025-06-13 10:23:21 +0100

    refactor(persistence): rename AddInfrastructure to AddPersistence
    - Renamed the AddInfrastructure extension method to AddPersistence in the Persistence layer for improved clarity and consistency.
    - Updated Program.cs to reflect the new method name.
    - Enhances readability and aligns naming with project structure.

--- AirlineBookingSystem.API/Program.cs
+++ AirlineBookingSystem.API/Program.cs
@@ -8,7 +8,7 @@ Env.Load();
 var builder = WebApplication.CreateBuilder(args);
 builder.Configuration
     .AddEnvironmentVariables();
-builder.Services.AddInfrastructure(builder.Configuration);
+builder.Services.AddPersistence(builder.Configuration);
 builder.Services.AddApplication();
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
