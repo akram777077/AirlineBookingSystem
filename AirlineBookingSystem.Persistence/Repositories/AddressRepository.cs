b8106c96ec428f5db67522fda8115eb61b679ae2
Hocine-Bec <Bechebil.Houcin@gmail.com>
2025-06-17 19:28:49 +0100

    feat(Persistence): Add GetByCountryIdAsync and GetByCityIdAsync to AddressRepository
    - Added GetByCountryIdAsync method to retrieve address by country ID.
    - Added GetByCityIdAsync method to retrieve address by city ID.

--- AirlineBookingSystem.Persistence/Repositories/AddressRepository.cs
+++ AirlineBookingSystem.Persistence/Repositories/AddressRepository.cs
@@ -1,12 +1,24 @@
 using AirlineBookingSystem.Application.Interfaces.Repositories;
 using AirlineBookingSystem.Domain.Entities;
 using AirlineBookingSystem.Persistence.DbContext;
+using Microsoft.EntityFrameworkCore;
 
 namespace AirlineBookingSystem.Persistence.Repositories;
 
 public class AddressRepository(ApplicationDbContext context)
     : GenericRepository<Address>(context), IAddressRepository
 {
-    
+    public async Task<Address?> GetByCountryIdAsync(int countryId)
+    {
+        return await Context.Addresses
+            .AsNoTracking()
+            .FirstOrDefaultAsync(c => c.CountryId == countryId);
+    }
+    public async Task<Address?> GetByCityIdAsync(int cityId)
+    {
+        return await Context.Addresses
+            .AsNoTracking()
+            .FirstOrDefaultAsync(c => c.CityId == cityId);
+    }
 }
 
