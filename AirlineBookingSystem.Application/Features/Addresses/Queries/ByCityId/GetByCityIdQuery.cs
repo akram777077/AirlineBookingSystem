9744ab0e9a5966fe0573363e48f258b780014a49
akram <akram7.dris7@gmail.com>
2025-06-18 06:15:05 +0100

    refactor(application,unit-tests): remove address query handlers and related tests
    - Deleted application query handlers for addresses: GetByCityId, GetByCountryId, and GetList, along with their queries.
    - Removed corresponding unit tests for address queries: GetByCityIdHandlerTests, GetByCountryIdHandlerTests, and GetListHandlerTests.
    - Cleans up unused features and tests related to address querying functionality.

--- AirlineBookingSystem.Application/Features/Addresses/Queries/ByCityId/GetByCityIdQuery.cs
+++
@@ -1,7 +0,0 @@
-using AirlineBookingSystem.Application.Features.Addresses.Queries.ByCountryId;
-using AirlineBookingSystem.Shared.DTOs.Addresses;
-using MediatR;
-
-namespace AirlineBookingSystem.Application.Features.Addresses.Queries.ByCityId;
-
-public record GetByCityIdQuery(int CityId) : IRequest<AddressDto?>;
--- AirlineBookingSystem.Application/Features/Addresses/Queries/ByCountryId/GetByCountryIdQuery.cs
+++
@@ -1,6 +0,0 @@
-using AirlineBookingSystem.Shared.DTOs.Addresses;
-using MediatR;
-
-namespace AirlineBookingSystem.Application.Features.Addresses.Queries.ByCountryId;
-
-public record GetByCountryIdQuery(int CountryId) : IRequest<AddressDto?>;
