bac7289f97e701656441bd7467be95d0bc77f8a8
akram <akram7.dris7@gmail.com>
2025-06-27 18:39:12 +0100

    test(unit): refactor GetAddressByIdHandlerTests to use FluentAssertions and test data factory
    - Replaced manual assertions with FluentAssertions for improved readability and expressiveness
    - Updated test setup to use AddressFactory for creating test data instead of static TestData methods
    - Adjusted mocks to use specific address instances for mapping and repository behavior
    - Ensured test coverage remains the same with more maintainable and concise code

--- dotnet-backend/AirlineBookingSystem.UnitTests/Features/Addresses/Queries/GetAddressByIdHandlerTests.cs
+++ dotnet-backend/AirlineBookingSystem.UnitTests/Features/Addresses/Queries/GetAddressByIdHandlerTests.cs
@@ -3,7 +3,9 @@ using AirlineBookingSystem.Application.Interfaces.Repositories;
 using AirlineBookingSystem.Domain.Entities;
 using AirlineBookingSystem.Shared.DTOs.Addresses;
 using AirlineBookingSystem.UnitTests.Common;
+using AirlineBookingSystem.UnitTests.Common.TestData;
 using AutoMapper;
+using FluentAssertions;
 using Moq;
 
 namespace AirlineBookingSystem.UnitTests.Features.Addresses.Queries;
@@ -14,17 +16,22 @@ public class GetAddressByIdHandlerTests
     public async Task Handle_WhenAddressExistsById_ReturnAddress()
     {
         //Arrange
-        var addresses = TestData.GetAddressesList();
-        var addressDto = TestData.GetAddressDto();
-        
+        var address = AddressFactory.Create(1, "City Center", "27000");
+        var addressDto = new AddressDto
+        {
+            Id = address.Id,
+            Street = address.Street,
+            ZipCode = address.ZipCode,
+            CityId = address.CityId
+        };
         var addressRepositoryMock = new Mock<IAddressRepository>();
         addressRepositoryMock
             .Setup(repo => repo.GetByIdAsync(1))
-            .ReturnsAsync(addresses.FirstOrDefault(a => a.Id == 1));
+            .ReturnsAsync(address);
 
         var mapperMock = new Mock<IMapper>();
-        mapperMock.Setup(m => m.Map<AddressDto>(It.IsAny<Address>()))
-            .Returns((Address address) => addressDto);
+        mapperMock.Setup(m => m.Map<AddressDto>(address))
+            .Returns(addressDto);
         
         var handler = new GetAddressByIdHandler(addressRepositoryMock.Object, mapperMock.Object);
         var query = new GetAddressByIdQuery(1);
@@ -33,9 +40,7 @@ public class GetAddressByIdHandlerTests
         var result = await handler.Handle(query, CancellationToken.None);
         
         //Assert
-        Assert.NotNull(result);
-        Assert.Equal("27000", result.ZipCode);
-        Assert.Equal("City Center", result.Street);
+        result.Should().BeEquivalentTo(addressDto);
     }
     
     [Fact]
@@ -55,6 +60,6 @@ public class GetAddressByIdHandlerTests
         var result = await handler.Handle(query, CancellationToken.None);
         
         //Assert
-        Assert.Null(result);
+        result.Should().BeNull();
     }
 }
