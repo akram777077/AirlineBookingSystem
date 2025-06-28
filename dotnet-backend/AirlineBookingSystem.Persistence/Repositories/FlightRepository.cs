edd0a8cf3fcb453a0e624d91fd5637f39911d5a4
akram <akram7.dris7@gmail.com>
2025-06-28 13:58:29 +0100

    feat(persistence): enhance FlightRepository to include related airport and city data in GetAllAsync
    - Override GetAllAsync to eagerly load related FromAirport and ToAirport entities along with their associated City data
    - Improve data retrieval for flights by reducing the need for additional queries when accessing airport and city information

--- dotnet-backend/AirlineBookingSystem.Persistence/Repositories/FlightRepository.cs
+++ dotnet-backend/AirlineBookingSystem.Persistence/Repositories/FlightRepository.cs
@@ -2,6 +2,7 @@ using AirlineBookingSystem.Application.Interfaces.Repositories;
 using AirlineBookingSystem.Domain.Entities;
 using AirlineBookingSystem.Persistence.DbContext;
 using AirlineBookingSystem.Persistence.Repositories.Generic;
+using AirlineBookingSystem.Shared.DTOs.Flights;
 using Microsoft.EntityFrameworkCore;
 
 namespace AirlineBookingSystem.Persistence.Repositories;
@@ -34,6 +35,14 @@ public class FlightRepository(ApplicationDbContext context)
         return await Context.Flights
             .Where(f => f.DepartureTime != null && f.DepartureTime.Value > fromDate)
             .OrderBy(f => f.DepartureTime)
+            .ToListAsync();
+    }
+
+    public override async Task<IReadOnlyList<Flight>> GetAllAsync()
+    {
+        return await Context.Flights
+            .Include(f => f.FromAirport).ThenInclude(a => a.City)
+            .Include(f => f.ToAirport).ThenInclude(a => a.City)
             .ToListAsync();
     }
 }
