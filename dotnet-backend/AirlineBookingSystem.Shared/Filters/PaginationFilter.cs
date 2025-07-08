c269c3419969cb62c4fc73f1d4ac5e3443a771f9
akram <akram7.dris7@gmail.com>
2025-07-08 21:51:03 +0100

    feat: Add pagination infrastructure

---
+++ dotnet-backend/AirlineBookingSystem.Shared/Filters/PaginationFilter.cs
@@ -0,0 +1,32 @@
+
+using System.Reflection;
+using System.Collections.Generic;
+
+namespace AirlineBookingSystem.Shared.Filters;
+
+public class PaginationFilter
+{
+    private const int MaxPageSize = 20;
+    private int _pageSize = 10;
+    public int PageNumber { get; set; } = 1;
+
+    public int PageSize
+    {
+        get => _pageSize;
+        set => _pageSize = value > MaxPageSize ? MaxPageSize : value;
+    }
+
+    public IDictionary<string, string> ToDictionary()
+    {
+        var dictionary = new Dictionary<string, string>();
+        foreach (var prop in GetType().GetProperties())
+        {
+            var value = prop.GetValue(this);
+            if (value != null)
+            {
+                dictionary.Add(prop.Name, value.ToString());
+            }
+        }
+        return dictionary;
+    }
+}
---
+++ dotnet-backend/AirlineBookingSystem.Shared/Results/PagedResult.cs
@@ -0,0 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace AirlineBookingSystem.Shared.Results;
+
+public class PagedResult<T> : Result<T>
+{
+    public int PageNumber { get; private set; }
+    public int PageSize { get; private set; }
+    public int TotalPages { get; private set; }
+    public int TotalRecords { get; private set; }
+
+    public PagedResult(T data, int pageNumber, int pageSize, int totalRecords) : base(data, null, ResultStatusCode.Success)
+    {
+        PageNumber = pageNumber;
+        PageSize = pageSize;
+        TotalRecords = totalRecords;
+        TotalPages = (int)Math.Ceiling(totalRecords / (double)pageSize);
+    }
+
+    public static async Task<PagedResult<List<T>>> ToPagedList(IQueryable<T> source, int pageNumber, int pageSize)
+    {
+        var count = await source.CountAsync();
+        var items = await source.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToListAsync();
+        return new PagedResult<List<T>>(items, pageNumber, pageSize, count);
+    }
+}
