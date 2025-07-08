b8e0f632b78898fcf3168c2e1242615809252846
akram <akram7.dris7@gmail.com>
2025-07-08 21:52:10 +0100

    refactor: Update SearchFlightsQuery and Handler for pagination

--- dotnet-backend/AirlineBookingSystem.Application/Features/Flights/Query/Search/SearchFlightsQueryHandler.cs
+++ dotnet-backend/AirlineBookingSystem.Application/Features/Flights/Query/Search/SearchFlightsQueryHandler.cs
@@ -3,17 +3,18 @@ using AirlineBookingSystem.Shared.DTOs.flights;
 using AirlineBookingSystem.Shared.Results;
 using AutoMapper;
 using MediatR;
+using AutoMapper.QueryableExtensions;
 
 namespace AirlineBookingSystem.Application.Features.Flights.Query.Search;
 
 public class SearchFlightsQueryHandler (IUnitOfWork unitOfWork, IMapper mapper)
-    : IRequestHandler<SearchFlightsQuery, Result<IReadOnlyList<FlightSearchResultDto>>>
+    : IRequestHandler<SearchFlightsQuery, PagedResult<List<FlightSearchResultDto>>>
 {
-    public async Task<Result<IReadOnlyList<FlightSearchResultDto>>> Handle(SearchFlightsQuery request, CancellationToken cancellationToken)
+    public async Task<PagedResult<List<FlightSearchResultDto>>> Handle(SearchFlightsQuery request, CancellationToken cancellationToken)
     {
-        var flights = await unitOfWork.Flights.GetFlightsWithDetailsAsync(request.Filter);
-        var flightSearchResultDtos = mapper.Map<List<FlightSearchResultDto>>(flights);
-        return Result<IReadOnlyList<FlightSearchResultDto>>.Success(flightSearchResultDtos);
+        var flightsQuery = unitOfWork.Flights.GetFlightsWithDetails(request.Filter);
+        var flightSearchResultDtosQuery = flightsQuery.ProjectTo<FlightSearchResultDto>(mapper.ConfigurationProvider);
+        return await flightSearchResultDtosQuery.ToPagedResult(request.Filter.PageNumber, request.Filter.PageSize);
         
     }
 }
