4072137d5df644dddb5f47d629b9429aad1b5127
akram <akram7.dris7@gmail.com>
2025-09-04 15:09:42 +0100

    Refactor: Update LoginUserQuery and Handler to return Result<User>

--- dotnet-backend/AirlineBookingSystem.Application/Features/Auth/Queries/Login/LoginUserQueryHandler.cs
+++ dotnet-backend/AirlineBookingSystem.Application/Features/Auth/Queries/Login/LoginUserQueryHandler.cs
@@ -1,24 +1,25 @@
 using AirlineBookingSystem.Application.Interfaces.UnitOfWork;
 using AirlineBookingSystem.Shared.Results;
 using MediatR;
+using AirlineBookingSystem.Domain.Entities;
 
 namespace AirlineBookingSystem.Application.Features.Auth.Queries.Login;
 
-public class LoginUserQueryHandler(IUnitOfWork unitOfWork) : IRequestHandler<LoginUserQuery, Result>
+public class LoginUserQueryHandler(IUnitOfWork unitOfWork) : IRequestHandler<LoginUserQuery, Result<User>>
 {
-    public async Task<Result> Handle(LoginUserQuery request, CancellationToken cancellationToken)
+    public async Task<Result<User>> Handle(LoginUserQuery request, CancellationToken cancellationToken)
     {
         var user = await unitOfWork.Users.GetUserWithPersonAsync(request.Username);
         if (user == null)
         {
-            return Result.NotFound("User not found.");
+            return Result<User>.NotFound("User not found.");
         }
 
         if (!BCrypt.Net.BCrypt.Verify(request.Password, user.Password))
         {
-            return Result.Unauthorized("Invalid credentials.");
+            return Result<User>.Unauthorized("Invalid credentials.");
         }
 
-        return Result.Success();
+        return Result<User>.Success(user);
     }
 }
