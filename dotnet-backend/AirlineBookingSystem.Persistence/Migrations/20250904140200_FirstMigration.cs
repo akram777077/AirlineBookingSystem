40a0750b247ee47aa0fa2f25c27af2bb9fd5590a
akram <akram7.dris7@gmail.com>
2025-09-04 15:10:50 +0100

    Feat: Add FirstMigration

---
+++ dotnet-backend/AirlineBookingSystem.Persistence/Migrations/20250904140200_FirstMigration.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace AirlineBookingSystem.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class FirstMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "payments");
+
+            migrationBuilder.DropTable(
+                name: "bookings");
+
+            migrationBuilder.CreateTable(
+                name: "RefreshTokens",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    UserId = table.Column<int>(type: "integer", nullable: false),
+                    Token = table.Column<string>(type: "text", nullable: false),
+                    Expires = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Created = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Revoked = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RefreshTokens", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_RefreshTokens_users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RefreshTokens_UserId",
+                table: "RefreshTokens",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "RefreshTokens");
+
+            migrationBuilder.CreateTable(
+                name: "bookings",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    booking_status_id = table.Column<int>(type: "integer", nullable: false),
+                    flight_id = table.Column<int>(type: "integer", nullable: false),
+                    seat_id = table.Column<int>(type: "integer", nullable: true),
+                    user_id = table.Column<int>(type: "integer", nullable: false),
+                    booked_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false, defaultValueSql: "CURRENT_TIMESTAMP"),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false, defaultValueSql: "CURRENT_TIMESTAMP"),
+                    deleted_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    payment_status = table.Column<string>(type: "text", nullable: false),
+                    ticket_number = table.Column<string>(type: "text", nullable: true),
+                    updated_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_bookings", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_bookings_booking_status_booking_status_id",
+                        column: x => x.booking_status_id,
+                        principalTable: "booking_status",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_bookings_flights_flight_id",
+                        column: x => x.flight_id,
+                        principalTable: "flights",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_bookings_seats_seat_id",
+                        column: x => x.seat_id,
+                        principalTable: "seats",
+                        principalColumn: "id");
+                    table.ForeignKey(
+                        name: "FK_bookings_users_user_id",
+                        column: x => x.user_id,
+                        principalTable: "users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "payments",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    booking_id = table.Column<int>(type: "integer", nullable: false),
+                    amount = table.Column<decimal>(type: "numeric(18,2)", nullable: false),
+                    paid_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    method = table.Column<string>(type: "text", nullable: false),
+                    transaction_id = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_payments", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_payments_bookings_booking_id",
+                        column: x => x.booking_id,
+                        principalTable: "bookings",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_bookings_booking_status_id",
+                table: "bookings",
+                column: "booking_status_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_bookings_flight_id",
+                table: "bookings",
+                column: "flight_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_bookings_seat_id",
+                table: "bookings",
+                column: "seat_id",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_bookings_ticket_number",
+                table: "bookings",
+                column: "ticket_number",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_bookings_user_id_flight_id",
+                table: "bookings",
+                columns: new[] { "user_id", "flight_id" },
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_payments_booking_id",
+                table: "payments",
+                column: "booking_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_payments_transaction_id",
+                table: "payments",
+                column: "transaction_id",
+                unique: true);
+        }
+    }
+}
