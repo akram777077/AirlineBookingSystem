7d6b899fbf720715b65199252b91198af40dfde3
akram <akram7.dris7@gmail.com>
2025-09-04 18:24:57 +0100

    Feat: Implement caching in GetFlightByIdQueryHandler

--- dotnet-backend/AirlineBookingSystem.Application/Features/Flights/Queries/GetById/GetFlightByIdQueryHandler.cs
+++ dotnet-backend/AirlineBookingSystem.Application/Features/Flights/Queries/GetById/GetFlightByIdQueryHandler.cs
@@ -3,15 +3,21 @@ using AirlineBookingSystem.Shared.DTOs.flights;
 using AirlineBookingSystem.Shared.Results;
 using AutoMapper;
 using MediatR;
+using AirlineBookingSystem.Application.Interfaces.Services;
 
 namespace AirlineBookingSystem.Application.Features.Flights.Queries.GetById;
 
-public class GetFlightByIdQueryHandler (IUnitOfWork unitOfWork, IMapper mapper)
+public class GetFlightByIdQueryHandler (IUnitOfWork unitOfWork, IMapper mapper, ICacheService cacheService)
     : IRequestHandler<GetFlightByIdQuery, Result<FlightDetailsDto>>
 {
     public async Task<Result<FlightDetailsDto>> Handle(GetFlightByIdQuery request, CancellationToken cancellationToken)
     {
-        var flight = await unitOfWork.Flights.GetByIdAsync(request.Id);
+        var cacheKey = $"Flight:{request.Id}";
+        var flight = await cacheService.GetOrCreateAsync(cacheKey, async () =>
+        {
+            return await unitOfWork.Flights.GetByIdAsync(request.Id);
+        }, TimeSpan.FromMinutes(5)); // Cache for 5 minutes
+
         if (flight == null)
         {
             return Result<FlightDetailsDto>.Failure("Flight not found", ResultStatusCode.NotFound);
