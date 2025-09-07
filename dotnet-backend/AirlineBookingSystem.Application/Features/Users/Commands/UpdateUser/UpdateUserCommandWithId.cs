1749d5fb577f237e961461c37839efc56ef0acd5
akram <akram7.dris7@gmail.com>
2025-09-07 13:37:27 +0100

    feat(user): Add UpdateUserCommandWithId

--- dotnet-backend/AirlineBookingSystem.Application/Features/Users/Commands/UpdateUser/UpdateUserCommandWithId.cs
+++ dotnet-backend/AirlineBookingSystem.Application/Features/Users/Commands/UpdateUser/UpdateUserCommandWithId.cs
@@ -3,8 +3,23 @@ using MediatR;
 
 namespace AirlineBookingSystem.Application.Features.Users.Commands.UpdateUser;
 
+/// <summary>
+/// Represents a command to update an existing user's details, including the user's ID.
+/// </summary>
+/// <param name="Id">The unique identifier of the user to update.</param>
+/// <param name="Username">The updated username for the user.</param>
+/// <param name="Email">The updated email address of the user.</param>
+/// <param name="FirstName">The updated first name of the user.</param>
+/// <param name="LastName">The updated last name of the user.</param>
+/// <param name="MidName">The updated middle name of the user (optional).</param>
+/// <param name="DateOfBirth">The updated date of birth of the user.</param>
+/// <param name="GenderId">The updated ID of the gender of the user.</param>
+/// <param name="Street">The updated street address of the user.</param>
+/// <param name="CityId">The updated ID of the city where the user resides.</param>
+/// <param name="ZipCode">The updated zip code of the user's address.</param>
+/// <param name="RoleId">The updated ID of the role assigned to the user.</param>
 public record UpdateUserCommandWithId(
-    int UserId,
+    int Id,
     string Username,
     string Email,
     string FirstName,
